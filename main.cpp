8bf28ecd7b14ebda29c6b42a4874ab2db30dd3e3
Ankit Khatiwada <51513779+ch-ankit@users.noreply.github.com>
2019-07-09 23:28:45 +0545

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
-#include "test.h"
-#include <QApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int main()
 {
-    QApplication a(argc, argv);
-    test w;
-    w.show();
-
-    return a.exec();
+    double bvcs=0.00167;
+    double vwall[3];
+    vwall[0]=1;
+    double vbrk=0.00157;
+    int nbrick[3];
+    nbrick[0]=(vwall[0]/bvcs);
+    int bprice=5.5;
+    double cemvol=0.0347;
+    double ppc=410;
+    double sndvol=7.50396;
+    double brick;
+    brick=nbrick[0]*bprice;
+    double vcs;
+    vcs=nbrick[0]*(bvcs-vbrk);
+    double vcem=vcs/7;
+    double vsnd=(6*vcs)/7;
+    double cost;
+    cost=brick+((vcem/cemvol)*ppc)+((vsnd/sndvol)*2251);
+    cout<<cost;
+    return 0;
 }
